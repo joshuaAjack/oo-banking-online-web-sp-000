77187f31207cd8c15f05252e111a3fe6c09214ed
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-13 01:51:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,36 @@
 class Transfer
-  attr_reader :Transfer, :sender, :receiver, :status
-  
-  def initialize(transfer)
-    @transfer = transfer
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount, status="pending")
     @sender = sender
-    @receiver = receiver  
-    @status = "pending"
+    @receiver = receiver
+    @amount = amount
+    @status = status
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."      
+    end
   end
 end
