5c062625dcc54fee2675997d352dbfddcd24fdc2
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-13 01:47:15 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+  attr_reader :transfer, :sender, :receiver, :status, :amount
+  
+  def initialize(transfer)
+    @transfer = amount 
+    @sender = sender
+    @receiver = receiver  
+    @status = "pending"
+  end
 end
