ce39c5513b929a6c4fc5667439383df18e0b8a5f
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-13 01:51:15 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -7,7 +7,23 @@ class BankAccount
     @status = "open"
   end
   
-  def self.deposit
-    @deposit
+def deposit(amount)
+    self.balance += amount
+  end
 
+  def withdrawal(amount)
+    self.balance -= amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
